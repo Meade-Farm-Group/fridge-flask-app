acb9db7b493b40831e6a19e13e7d6a05c37f49b0
TJ O'Hora <tjohora@MPC.local>
2022-01-05 09:55:36 +0000

    Fixed: search form validation from accepting only whitespace

--- forms.py
+++ forms.py
@@ -1,12 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, SubmitField, DateField
+from wtforms import StringField, IntegerField, SubmitField, DateField,\
+                    ValidationError
 from wtforms.validators import Optional, Length, DataRequired
+
+
+# def no_whitespace_only(form, field):
+#     if len(field.data.strip()) == 0:
+#         raise ValidationError('No Whitespace only')
 
 
 class SearchForm(FlaskForm):
     product_name = StringField('Product Name', validators=[
         Optional(strip_whitespace=True),
-        Length(min=1, max=50, message=None)])
+        Length(min=1, max=50, message=None),
+        ])
     purchase_order = IntegerField('Purchase Order Number', validators=[
         Optional(strip_whitespace=True)])
     reference = StringField('Reference', validators=[
@@ -17,10 +24,11 @@ class SearchForm(FlaskForm):
 
     def validate(self, extra_validators=None):
         if super().validate(extra_validators):
-
+            product_name = self.product_name.data.strip()
+            reference = self.reference.data.strip()
             # your logic here e.g.
-            if not (self.product_name.data or self.purchase_order.data or
-                    self.reference.data or self.best_before_date.data):
+            if not (product_name or self.purchase_order.data or
+                    reference or self.best_before_date.data):
                 self.product_name.errors.append(
                     'At least one field must have a value')
                 return False
