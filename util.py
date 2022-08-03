2fbb6a2475769a69563c1410b9921c7c8fbc751d
TJ O'Hora <tjohora@MPC.local>
2022-08-03 11:10:32 +0100

    Fix: updated check table type

--- util.py
+++ util.py
@@ -5,7 +5,8 @@ def check_table_type(cell_id):
     if("D2" in cell_id):
         tableType = "3d"
     elif(cell_id.count('-') == 2 or
-         "FVFR" in cell_id):
+         "FVFR" in cell_id or
+         "SRW" in cell_id):
         tableType = "2d"
     else:
         tableType = "3d"
